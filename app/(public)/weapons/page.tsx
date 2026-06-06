0af76a002ec62e39b4361b8b13697faeb935a01c
mtaufikbaskoro <mtaufikbaskoro@gmail.com>
2026-06-07 01:43:42 +0700

    add example image

--- app/(public)/weapons/page.tsx
+++ app/(public)/weapons/page.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Page = () => {
-    return <div>Weapon List Page</div>
+    return (
+            <div>
+                <Image
+                    src="https://assets.mhw-db.com/weapons/great-sword/icons/83b9e1fa727ca6ba922b53a42626a167.26ad6221e21811da5278502fabfc138b33d622bc.png"
+                    alt="great-sword-icon"
+                    width={40}
+                    height={40}
+                    loading='eager'
+                />
+            </div>
+        )
 }
 
 export default Page
